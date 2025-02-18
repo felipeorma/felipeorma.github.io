1b0ddc3ee37a486c4d5799db281753b35172804b
Felipe Ormazabal <97199418+felipeorma@users.noreply.github.com>
2025-02-18 13:26:55 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Para repositorio username.github.io, usamos '/'
+  base: '/', // Para repositorio username.github.io
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
